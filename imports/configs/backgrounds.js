3629e808a06e826bac545f0f56c999a942a47e65
Daniel Zapata <daniel@peoplepattern.com>
2018-06-20 13:19:01 -0500

    missed file

---
+++ imports/configs/backgrounds.js
@@ -0,0 +1,44 @@
+export const BACKGROUNDS = {
+  acolyte: {
+    label: 'Acolyte',
+    proficiencies: ['insight', 'religion'],
+    features: ['extra_language', 'extra_language'],
+    items: ['holy_symbol','prayer_book','incense_stick','incense_stick','incense_stick','incense_stick','incense_stick','vestments','common_clothes'],
+    wealth: 15,
+  },
+  criminal: {
+    label: 'Criminal',
+    proficiencies: ['deception', 'stealth', 'gaming','theives_tools'],
+    features: [],
+    items: ['crowbar','common_clothes'],
+    wealth: 15,
+  },
+  folk_hero: {
+    label: 'Folk Hero',
+    proficiencies: ['animal_handling', 'survival', 'artisan_tools','land_vehicles'],
+    features: [],
+    items: ['artisan_tools','shovel','iron_pot','common_clothes'],
+    wealth: 10,
+  },
+  noble: {
+    label: 'Noble',
+    proficiencies: ['persuasion', 'history', 'gaming'],
+    features: ['extra_language'],
+    items: ['fine_clothes','signet_ring','pedigree_scroll'],
+    wealth: 25,
+  },
+  sage: {
+    label: 'Sage',
+    proficiencies: ['arcana', 'history'],
+    features: ['extra_language','extra_language'],
+    items: ['ink_bottle','quill','small_knife','dead_colleague_letter','common_clothes'],
+    wealth: 10,
+  },
+  soldier: {
+    label: 'Soldier',
+    proficiencies: ['athletics', 'intimidation', 'gaming', 'land_vehicles'],
+    features: [],
+    items: ['rank_insignia','broken_blade','card_deck','common_clothes'],
+    wealth: 10,
+  },
+}
